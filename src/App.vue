55d23a8f5b75e1eed3a0f9c31df01bdd2b57e85b
unknown <chievmanin@gmail>
2023-02-22 19:48:15 +0700

    Add vue electron with tailwind

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <SampleTyping msg="Welcome to Your Vue.js App" />
+</template>
+
+<script setup>
+import SampleTyping from './components/Sample-item.vue';
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
